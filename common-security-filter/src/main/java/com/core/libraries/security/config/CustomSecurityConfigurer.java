cc0de081dcfaf7d2b755b840310b33232c5c622a
Bayvao Verma <34921943+Bayvao@users.noreply.github.com>
2022-10-07 00:41:18 +0530

    changed security configuration to automatically pick whitelisted public URIs from properties file and configure it.

---
+++ common-security-filter/src/main/java/com/core/libraries/security/config/CustomSecurityConfigurer.java
@@ -0,0 +1,69 @@
+package com.core.libraries.security.config;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.util.ObjectUtils;
+
+import java.util.Iterator;
+import java.util.Map;
+
+@Data
+@Configuration
+public class CustomSecurityConfigurer {
+
+    @Value("#{${com.food.swipe.publicURIs:{:}}}")
+    private Map<String, String> publicURIs;
+
+    @Value(("#{${com.food.swipe.techinalURIs:{'/**/actuator/**':'GET','/swagger-ui/**':'GET'}}}"))
+    private Map<String, String> publicTechnicalURIs;
+
+    @Bean
+    public MultiValueMap<String, String> allListedPublicURIs(){
+        MultiValueMap<String, String> allPublicURIs = new LinkedMultiValueMap<>();
+
+        Iterator iterator;
+        Map.Entry currentEntry;
+        String[] allListedURIs;
+        int len;
+        String currentHttpMethod;
+
+        if(!ObjectUtils.isEmpty(this.publicURIs)){
+            iterator = this.publicURIs.entrySet().iterator();
+
+            while (iterator.hasNext()){
+                currentEntry = (Map.Entry) iterator.next();
+                allListedURIs = ((String)currentEntry.getValue()).split(",");
+                len = allListedURIs.length;
+
+                for (int i = 0; i < len; ++i){
+                    currentHttpMethod = allListedURIs[i];
+                    allPublicURIs.add((String) currentEntry.getKey(), currentHttpMethod);
+                }
+            }
+        }
+
+        if(!ObjectUtils.isEmpty(publicTechnicalURIs)){
+            iterator = this.publicTechnicalURIs.entrySet().iterator();
+
+            while (iterator.hasNext()){
+                currentEntry = (Map.Entry) iterator.next();
+                allListedURIs = ((String)currentEntry.getValue()).split(",");
+                len = allListedURIs.length;
+
+                for (int i = 0; i < len; ++i){
+                    currentHttpMethod = allListedURIs[i];
+                    allPublicURIs.add((String) currentEntry.getKey(), currentHttpMethod);
+                }
+            }
+
+        }
+
+        return  allPublicURIs;
+    }
+
+}
--- common-security-filter/src/main/java/com/core/libraries/security/config/SecurityConfig.java
+++ common-security-filter/src/main/java/com/core/libraries/security/config/SecurityConfig.java
@@ -2,16 +2,21 @@ package com.core.libraries.security.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.util.MultiValueMap;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -22,31 +27,58 @@ import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 public class SecurityConfig {
 
-    private static final Logger logger = LoggerFactory.getLogger(SecurityConfig.class);
+    @Autowired
+    private CustomSecurityConfigurer customSecurityConfigurer;
 
+    @Autowired
+    private MultiValueMap<String, String> allListedPublicURIs;
+
+    private static final Logger logger = LoggerFactory.getLogger(SecurityConfig.class);
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
         logger.debug("SecurityConfig initialized.");
-        // We don't need CSRF for this example
-        httpSecurity.csrf().disable()
-                .authorizeRequests().anyRequest().permitAll().and()
-                // don't authenticate this particular request
-                //.authorizeHttpRequests().antMatchers("/","/authenticate","/user/create").permitAll()
+
+        ExpressionUrlAuthorizationConfigurer<HttpSecurity>
+                .ExpressionInterceptUrlRegistry authorizeRequests = httpSecurity.authorizeRequests();
+
+        // don't authenticate this particular uri requests
+        addPublicURIs(authorizeRequests);
+
+        authorizeRequests
                 // all other requests need to be authenticated
-                // .anyRequest().authenticated().and()
+                 .anyRequest().authenticated().and()
                 // make sure we use stateless session; session won't be used to
                 // store user's state.
                 // .exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().csrf().disable().cors();
 
         // Add a filter to log the request-response of every request
-        // httpSecurity.addFilterBefore(loggingFilter, UsernamePasswordAuthenticationFilter.class);
+        //httpSecurity.addFilterBefore(loggingFilter, UsernamePasswordAuthenticationFilter.class);
         // Add a filter to validate the tokens with every request
         //httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
         return httpSecurity.build();
+    }
+
+    public void addPublicURIs(ExpressionUrlAuthorizationConfigurer<HttpSecurity>
+                                      .ExpressionInterceptUrlRegistry authorizeRequests){
+
+        if(!ObjectUtils.isEmpty(allListedPublicURIs)){
+            allListedPublicURIs.keySet().stream()
+                    .forEach(uri -> whitelistAllURI(authorizeRequests, uri));
+        }
+    }
+
+    private void whitelistAllURI(ExpressionUrlAuthorizationConfigurer<HttpSecurity>
+                                         .ExpressionInterceptUrlRegistry authorizeRequests, String uri) {
+
+        for (String currentMethod : allListedPublicURIs.get(uri)){
+            authorizeRequests.antMatchers(currentMethod, uri).permitAll();
+        }
     }
 
     @Bean
